c3cd966d84dd184f9eb09854d9fe04b3c9ee1288
johnleung528 <39686681+johnleung528@users.noreply.github.com>
2018-06-29 18:11:00 +0800

    win page styling

--- src/app/Win.js
+++ src/app/Win.js
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
 import './Win.css'
 import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 import MenuIcon from '@material-ui/icons/Menu';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+
+const getInfo = [
+  ['rn', '主動計劃並實踐，向特定對象打開福音話題之人次'],
+  ['mc', '隨機佈道打開福音話題之人次'],
+  ['inv', '邀請對象接受基督成為救主之人次'],
+  ['prc', '接受基督成為救主之人數'],
+  ['hsb', '分享被聖靈充滿真理之對象人次'],
+  ['ahs', '願意即時禱告被聖靈充滿之人次']
+]
 
 class Win extends Component {
   constructor (props) {
     super(props);
     this.state = {
       open: false,
-      stat: {
-        rn: 0,
-        mc: 0,
-        inv: 0,
-        prc: 0,
-        hsb: 0,
-        ahs: 0
-      }
+      takeMethod: 'add',
+      collect_tab: 'Win',
+      week: '2018-06-25',
+      peopleGroup: 'local',
+      campus: 'Cat',
+      stat: [0, 0, 0, 0, 0, 0]
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -33,7 +37,12 @@ class Win extends Component {
     this.handlePlus = this.handlePlus.bind(this);
     this.toggleDrawer = this.toggleDrawer.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
 
+  }
+
+  handleSelect(event) {
+    this.setState({peopleGroup: event.target.value});
   }
 
   handleClick(event) {
@@ -41,9 +50,10 @@ class Win extends Component {
   }
 
   handleChange(event) {
-    this.setState({
-      stat: {rn: event.target.value}
-    });
+    let arr = this.state.stat;
+    let index = event.target.id.toInt();
+    arr[index] = event.target.value;
+    this.setState({stat: arr});
   }
 
   handleSubmit(event) {
@@ -51,16 +61,16 @@ class Win extends Component {
     this.setState({stat: {startFu: 0}});
   }
 
-  handleMinus() {
-    let value = this.state.stat.rn;
-    value = value > 0 ? --value : value;
-    this.setState({stat: {rn: value}});
+  handleMinus(index) {
+    let arr = this.state.stat;
+    arr[index] = arr[index] === 0 ? arr[index] : --arr[index];
+    this.setState({stat: arr});
   }
 
-  handlePlus() {
-    let value = this.state.stat.rn;
-    value++;
-    this.setState({stat: {rn: value}})
+  handlePlus(index) {
+    let arr = this.state.stat;
+    arr[index]++;
+    this.setState({stat: arr});
   }
 
   toggleDrawer() {
@@ -68,35 +78,59 @@ class Win extends Component {
   }
 
   render() {
+    const appList = getInfo.map((app, index) => (
+      <tr key={index} className='appList'>
+        <td className='appList-td'>
+          {app[0].toUpperCase()}
+          <br />
+          {app[1]}
+        </td>
+        <td className='appList-td' style={{textAlign: 'center'}}>
+          <button onClick={() => {this.handleMinus(index)}} className='ripple app-button'><RemoveIcon /></button>
+          <TextField
+            id={index.toString()}
+            className='statInput'
+            value={this.state.stat[index]}
+            onChange={this.handleChange}
+            type="number"
+            onClick={this.handleClick}
+            margin="dense"
+          />
+        <button onClick={() => {this.handlePlus(index)}} className='ripple app-button'><AddIcon /></button>
+        </td>
+      </tr>
+    ));
 
     return (
         <div className="win">
           <Paper className="win-header">
             <div className="header-box">
               <h3 style={{display: 'inline-block', position: 'relative', width: '50%', marginTop: '0.5em'}}>Hi! John</h3>
-              <h3 style={{display: 'inline-block', position: 'relative', width: '50%'}}>Hccccccci!</h3>
+              <h3 style={{display: 'inline-block', position: 'relative', width: '50%'}}>{this.state.week}</h3>
             </div>
             <div className="header-box">
               <button
                 onClick={this.toggleDrawer}
-                style={{display: 'block', position: 'relative', float: 'left', boxSizing: 'border-box', border: 'none', height: '100%', width: '50%', padding: '0', margin: '0', backgroundColor: 'red'}}
+                style={{display: 'block', position: 'relative', float: 'left', boxSizing: 'border-box', border: 'none', height: '100%', width: '50%', padding: '0', margin: '0', backgroundColor: 'red', textAlign: 'left'}}
+                className='ripple drawer-button'
               >
                 <MenuIcon
-                 style={{display: 'block', position: 'relative', fontSize: '3em', padding: '0', margin: '0', height: '100%'}} />  {this.state.stat.rn}
+                 style={{display: 'inline-block', position: 'relative', fontSize: '3em', padding: '0', margin: '0', height: '100%', float: 'left'}} />
+               <span style={{display: 'inline-block', boxSizing: 'border-box', fontSize: '1.5em', width: 'auto', height: '5vh', textAlign: 'center', lineHeight: '5vh', margin: '0', top: 'auto'}}>{this.state.campus}</span>
               </button>
 
               <Select
-                value={this.state.stat.rn}
-                onChange={this.handleChange}
+                value={this.state.peopleGroup}
+                onChange={this.handleSelect}
                 inputProps={{
                    name: 'People Group',
                    id: 'people-group',
                 }}
                 style={{display: 'inline-block', position: 'relative', width: '50%', height: '90%'}}
               >
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+                <MenuItem value={'local'}>Local</MenuItem>
+                <MenuItem value={'international'}>International</MenuItem>
+                <MenuItem value={'bridge'}>Bridge</MenuItem>
               </Select>
             </div>
           </Paper>
@@ -110,19 +144,13 @@ class Win extends Component {
             </div>
           </Drawer>
 
-          <section>
-            RN
-            <button onClick={this.handleMinus} >minus</button>
-            <TextField
-              id="number"
-              value={this.state.stat.rn}
-              onChange={this.handleChange}
-              type="number"
-              onClick={this.handleClick}
-              margin="dense"
-            />
-            <button onClick={this.handlePlus} >plus</button>
-
+          <section className='win-section'>
+            <table>
+              <tbody>
+                {appList}
+              </tbody>
+            </table>
+            <button className='submit'>Submit</button>
           </section>
         </div>
     );
